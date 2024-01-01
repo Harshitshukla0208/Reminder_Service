dd19d630d4c82fdaaf6ddd0b3a31b00104c3830b
Harshit Shukla <mrharshit2409@gmail.com>
2024-01-01 23:10:34 +0530

    Setup of SMTP using nodemailer completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig')
+const { sendBasicEmail } = require('./services/email-service')
 
 const setupAndStartServer = () => {
     const app = express();
@@ -8,7 +9,13 @@ const setupAndStartServer = () => {
     app.use(bodyParser.urlencoded({extended: true}));
 
     app.listen(PORT , () => {
-        console.log(`Server Started at port ${PORT}`)
+        console.log(`Server Started at port ${PORT}`);
+
+        sendBasicEmail('#@gmail.com',
+        '#@gmail.com',
+        'This is a testing email',
+        'Hello'
+        )
     })
 }
 
